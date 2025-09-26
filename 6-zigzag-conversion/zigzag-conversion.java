d23f9342fe10484b51738cda95baa724d74be632
Pulkit <choprapulkitt@gmail.com>
2025-09-26 17:32:28 +0530

    Time: 12 ms (21.11%) | Memory: 45.6 MB (26.65%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,49 +1,20 @@
  import java.util.*;
 class Solution {
     public String convert(String s, int numRows) {
-      
-
-        if (numRows == 1 || numRows >= s.length()) return s;
+        if (numRows==1) return s;
         int n = s.length();
-        char[][] mat = new char[numRows][n];
-        for (int i = 0; i < numRows; i++) {
-            Arrays.fill(mat[i], '\0');
-        }
-        int r = 0, c = 0;
-        boolean flag = true;
-
-        for (int i = 0; i < n; i++) {
-            mat[r][c] = s.charAt(i);
-            if (flag) {
-                if (r == numRows - 1) {
-                    flag = false;
-                    r--;
-                    c++;
-                } else {
-                    r++;
-                }
-            } else {
-                if (r == 0) {
-                    flag = true;
-                    r++;
-                } else {
-                    r--;
-                    c++;
-                }
+        int rc= numRows;
+        int jump = 2*(rc-1);
+        String res ="";
+        for(int i=0;i<rc;i++ ){
+        for(int j=i;j<n;j+=jump){
+            res+=s.charAt(j);
+            if(i!=0 && i!=rc-1 && j+jump-(2*i)<n){
+                res+=s.charAt(j+jump-(2*i));
             }
         }
-
-        
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] != '\0') {
-                    result.append(mat[i][j]);
-                }
-            }
         }
-
-        return result.toString();
-    
+       
+     return res;
     }
 }
