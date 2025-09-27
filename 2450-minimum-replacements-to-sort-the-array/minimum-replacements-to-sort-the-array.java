9652dd6da3261e454a4cc1ded523b8f57e0f29ca
Pulkit <choprapulkitt@gmail.com>
2025-09-27 15:36:28 +0530

    Time: 4 ms (93.33%) | Memory: 55.9 MB (37.78%) - LeetSync

--- 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.java
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.java
@@ -4,12 +4,14 @@ class Solution {
         long count =0;
         int n = nums.length;
         for(int i =n-2;i>=0;i--){
-            if(nums[i]>nums[i+1]){
-               int k = (int)Math.ceil((double)nums[i] / nums[i+1]);
-               int min = (int)(nums[i]/k);
-               nums[i]=min;
-               count+=k-1;
-            }
+            if(nums[i]%nums[i+1]==0){
+                    count+=(nums[i]/nums[i+1] -1);
+                    nums[i]=nums[i+1];
+                } else {
+                    int k = (nums[i]/nums[i+1])+1;
+                    count+=k-1;
+                    nums[i]=nums[i]/k;
+                }
         }
         return count;
     }
