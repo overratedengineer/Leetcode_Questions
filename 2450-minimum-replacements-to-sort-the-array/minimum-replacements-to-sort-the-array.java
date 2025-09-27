6c7aa5d343ee8870a7ecfb09491a98cee4ae6515
Pulkit <choprapulkitt@gmail.com>
2025-09-27 15:37:35 +0530

    Time: 5 ms (61.33%) | Memory: 56.2 MB (8.00%) - LeetSync

--- 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.java
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.java
@@ -4,6 +4,7 @@ class Solution {
         long count =0;
         int n = nums.length;
         for(int i =n-2;i>=0;i--){
+            if(nums[i]>nums[i+1]){
             if(nums[i]%nums[i+1]==0){
                     count+=(nums[i]/nums[i+1] -1);
                     nums[i]=nums[i+1];
@@ -11,7 +12,8 @@ class Solution {
                     int k = (nums[i]/nums[i+1])+1;
                     count+=k-1;
                     nums[i]=nums[i]/k;
-                }
+                } 
+            } else continue;
         }
         return count;
     }
