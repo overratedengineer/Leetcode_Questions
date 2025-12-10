f9f28913c6d574dd638f54c695b4e723fd626849
Pulkit <choprapulkitt@gmail.com>
2025-12-10 10:29:18 +0530

    Time: 3 ms (85.79%) | Memory: 54.4 MB (8.35%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,16 +1,16 @@
 class Solution {
-    public void dfs(int i , int j , char grid[][] , int vis[][]){
+    public void dfs(int i , int j , char grid[][]){
         int m=grid.length;
         int n=grid[0].length;
-        vis[i][j]=1;
+        grid[i][j]='0';
         int x[] = {1,-1,0,0};
         int y[] = {0,0,1,-1};
         for(int k =0;k<4;k++){
             int xi = i + x[k];
             int yj = j + y[k];
             if(xi<m && xi>=0 && yj<n &&yj>=0 ){
-                if(grid[xi][yj]=='1' && vis[xi][yj]!=1){
-                 dfs(xi,yj,grid,vis);
+                if(grid[xi][yj]=='1'){
+                 dfs(xi,yj,grid);
                 }
             }
         }
@@ -22,8 +22,8 @@ class Solution {
         int vis[][] = new int[m][n]; 
        for(int i =0; i<m ;i++){
         for(int j =0;j<n;j++){
-           if(vis[i][j]!=1 && grid[i][j]=='1'){
-            dfs(i,j,grid,vis);
+           if(grid[i][j]=='1'){
+            dfs(i,j,grid);
              count++;
            }
         }
