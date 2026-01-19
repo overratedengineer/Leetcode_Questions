b4344e579a8ca4d8e697775166a365da92e72c71
Pulkit <choprapulkitt@gmail.com>
2026-01-19 15:30:21 +0530

    Time: 1 ms (96.51%) | Memory: 42.8 MB (29.71%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+        int s =0;
+        int e =x/2;
+        if(x==1){return 1;}
+        if(x==0){return 0;}
+        while(s<=e){
+            double mid = (s+e)/2;
+
+            if(mid*mid==x){
+                return (int)mid;
+            } else if(mid*mid>x){
+            e=(int)mid-1;
+            } else if(mid*mid<x) {
+              s=(int)mid+1;
+            } 
+        }
+        return s-1;
+    }
+}
