c6556f45ece2730c5ceab57aaf85fcc1889061a1
Pulkit <choprapulkitt@gmail.com>
2026-01-19 16:20:05 +0530

    Time: 8 ms (73.08%) | Memory: 48.2 MB (25.74%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,28 +1,23 @@
 class Solution {
     public int minEatingSpeed(int[] arr, int h) {
-        int s =1;
-        int max=0;
-       for(int i=0;i<arr.length;i++){
-        max = Math.max(max,arr[i]);
-       }
-        int e = max;
-        while(s<=e){
-            int mid =s+(e-s)/2;
-            long val=0;
-            for(int i=0;i<arr.length;i++){
-             if(arr[i]<=mid){
-               val++;
-             } else if(arr[i]%mid==0){
-               val+=(arr[i]/mid);
-             } else {
-                 val+=(arr[i]/mid);
-                 val++;
-             }
+        int s = 1, e = 0;
+        // this is clean code
+        for (int x : arr) {
+            e = Math.max(e, x);
+        }
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            long hours = 0;
+
+            for (int x : arr) {
+                hours += (x + mid - 1) / mid;
             }
-            if(val>h){
-               s=mid+1;
-            } else if(val<=h){
-                e=mid-1;
+
+            if (hours > h) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
             }
         }
         return s;
