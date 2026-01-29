1534d28dbc0740eaac005f96619918ef7452ef61
Pulkit <choprapulkitt@gmail.com>
2026-01-29 16:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (43.84%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxDepth(String s) {
+      int count =0;
+      int max =0;
+      int n = s.length();
+      for(int i=0;i<n;i++){
+        if(s.charAt(i)=='(') {count++; max =Math.max(count,max); }
+        else if(s.charAt(i)==')'){count--;}
+      }  
+      return max;
+    }
+}
