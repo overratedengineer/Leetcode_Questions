895a100a14f8bc5afd0a7424133a8ae96e534afa
Pulkit <choprapulkitt@gmail.com>
2026-02-26 19:55:38 +0530

    Time: 3 ms (87.50%) | Memory: 43.7 MB (16.01%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for(char c : s.toCharArray()){
+
+            if(c == '}'){
+                if(!st.isEmpty()){
+                    char d = st.pop();
+                    if(d != '{') return false;
+                } else return false;
+            }
+
+            else if(c == ')'){
+                if(!st.isEmpty()){
+                    char d = st.pop();
+                    if(d != '(') return false;
+                } else return false;
+            }
+
+            else if(c == ']'){
+                if(!st.isEmpty()){
+                    char d = st.pop();
+                    if(d != '[') return false;
+                } else return false;
+            }
+
+            else{
+                st.push(c);
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
