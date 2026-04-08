fd92871c77940e581368f1909f43d45c42919a7a
Pulkit <choprapulkitt@gmail.com>
2026-04-08 17:03:12 +0530

    Time: 654 ms (5.08%) | Memory: 236.8 MB (17.38%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,33 +1,22 @@
 class Solution {
-
-    public int func(int i, int prevIndex, int[] arr, int[][] dp) {
-        if (i == arr.length) {
+    public int solve(int i,int p,int[] nums,int dp[][]){
+        if(i==nums.length){
             return 0;
         }
-        if (dp[i][prevIndex + 1] != -1) {
-            return dp[i][prevIndex + 1];
+        if(dp[i][p+1]!=0){
+            return dp[i][p+1]; 
         }
-
-        int take = 0;
-        if (prevIndex == -1 || arr[i] > arr[prevIndex]) {
-            take = 1 + func(i + 1, i, arr, dp);
-        }
-        int skip = func(i + 1, prevIndex, arr, dp);
-        dp[i][prevIndex + 1] = Math.max(take, skip);
-
-        return dp[i][prevIndex + 1];
+         int a = solve(i+1,p,nums,dp);
+         int b =0;
+         if(p==-1 || nums[i]>nums[p]){
+           b =solve(i+1,i,nums,dp)+1;
+         }
+     dp[i][p+1]=Math.max(a,b);
+     return dp[i][p+1];
     }
-
     public int lengthOfLIS(int[] nums) {
-
         int n = nums.length;
-        int[][] dp = new int[n][n + 1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= n; j++) {
-                dp[i][j] = -1;
-            }
-        }
-
-        return func(0, -1, nums, dp);
+        int dp[][] = new int[n][n+1];
+         return solve(0,-1,nums,dp);
     }
 }
