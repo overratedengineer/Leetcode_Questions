a9d2b5c3a9ff7808bfefbe334f50593c7ce82197
Pulkit <choprapulkitt@gmail.com>
2026-04-10 11:56:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (5.66%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,28 +1,28 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        int[][] dp = new int[m][n];
-
-        if (grid[0][0] == 1) {
-            return 0;
-        }
-        dp[0][0] = 1;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
-                    if (i > 0) {
-                        dp[i][j] += dp[i - 1][j];
-                    }
-                    if (j > 0) {
-                        dp[i][j] += dp[i][j - 1];
-                    }
-                }
-            }
-        }
-        return dp[m - 1][n - 1];
+    public int solve(int i,int j,int arr[][],Integer dp[][]){
+        int n = arr.length;
+        int m = arr[0].length;
+         if(arr[i][j]==1) {return 0;}
+      if(i==n-1 && j==m-1){
+       return 1;
+      }
+     // if(arr[i][j]==1) {return 0;}
+      if(dp[i][j]!=null) return dp[i][j] ;
+      int w =0;
+      if(i+1<n) {
+      w = solve(i+1,j,arr,dp);
+      }
+    if(j+1<m){
+       w+=solve(i,j+1,arr,dp);
     }
+    return dp[i][j]=w;
+    }
+    public int uniquePathsWithObstacles(int[][] arr) {
+        if (arr[0][0] == 1) return 0;
+          int n = arr.length;
+        int m = arr[0].length;
+        Integer dp[][] = new Integer[n][m];
+        return solve(0,0,arr,dp);
+    }
+
 }
