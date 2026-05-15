bc888f90f360967e7be5faa6ddfce859178e6ed8
Pulkit <choprapulkitt@gmail.com>
2026-05-15 17:32:03 +0530

    Time: 1601 ms (5.88%) | Memory: 95.3 MB (27.45%) - LeetSync

---
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minOperations(int[] arr) {
+     int n = arr.length;
+  int ss[] = new int[1000001];
+for(int i = 2; i*i <= 1000000; i++){
+    if(ss[i] == 0){
+        for(int j = i*i; j <= 1000000; j += i){
+            if(ss[j] == 0){
+                ss[j] = i;
+            }
+        }
+    }
+}
+      
+        
+        int count =0;
+       
+        for(int i = n-2; i >= 0; i--){
+            if(arr[i] > arr[i+1]){
+                if(ss[arr[i]] == 0){
+                    //System.out.println(-1);
+                    return -1;
+                }
+                arr[i] = ss[arr[i]];
+                count++;     
+            }
+            if(arr[i]>arr[i+1]){
+            //   if(ss[arr[i]] == 0){
+            //         //System.out.println(-1);
+            //         return -1;
+            //     }
+            return -1;
+            }
+        }
+        // for(int i: arr)System.out.println(i);
+        return count;
+}
+}
