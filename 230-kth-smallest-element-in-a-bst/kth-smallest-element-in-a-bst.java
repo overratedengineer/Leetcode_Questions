19fc177c02203c054ff615de20c9a89f6ff4488c
Pulkit <choprapulkitt@gmail.com>
2026-06-03 08:38:06 +0530

    Time: 1 ms (35.54%) | Memory: 46.9 MB (31.78%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> st = new Stack<>();
+        while (root != null || !st.isEmpty()) {
+            while (root != null) {
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            k--;
+            if (k == 0) {
+                return root.val;
+            }
+            root = root.right;
+        }
+        return -1;
+    }
+}
+//or do simple inorder
