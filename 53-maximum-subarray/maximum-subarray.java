371feddfd3e4981dc3f360f035e337eb16127058
Pulkit <choprapulkitt@gmail.com>
2026-07-16 14:23:31 +0530

    Time: 2 ms (4.35%) | Memory: 77.1 MB (82.67%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,17 +1,22 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // core idea if anywhere sum becomes negative leave it and move forward
-        int max =-9999999;
         int sum =0;
-         int maxi=-99999999;
-        for(int i:nums){
-            sum+=i;
-            if(sum<0) sum=0;
-            max = Math.max(max,sum);
-            maxi=Math.max(maxi,i);
-            
+        int n = nums.length;
+        int max =0;
+        int flag =0;;
+        int min = -100000;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(nums[i]>0){
+                flag=1;
+            }
+            if(sum<0){
+                sum=0;
+            }
+            max = Math.max(sum,max);
+            min = Math.max(nums[i],min);
         }
-        if(max==0) max=maxi;
+        if(flag==0) return min;
         return max;
     }
 }
