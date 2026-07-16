2bf2e6faea23858b7da8497f81302988f91e8996
Pulkit <choprapulkitt@gmail.com>
2026-07-16 16:21:46 +0530

    Time: 1 ms (98.63%) | Memory: 47.5 MB (62.63%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,45 +1,42 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-          // col matrix[i][0]
-          //row matrix[0][j] 
-          int col=1;
-
-          for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                 
-                if(matrix[i][j]==0){
-                     matrix[i][0]=0;
-                    if(j!=0){
-                    matrix[0][j]=0;
-                    } else {
-                      col=0;
-                    }
+    public void setZeroes(int[][] arr) {
+        int co =1;
+        int n = arr.length;
+        int m = arr[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+              if(arr[i][j]==0){
+                arr[i][0]=0;
+                if(j==0){
+                    co=0;
+                } else {
+                  arr[0][j]=0;
                 }
+              }
             }
-          }
-
-          for(int i=1;i<matrix.length;i++){
-            for(int j=1;j<matrix[0].length;j++){
-                if(matrix[i][0]==0||matrix[0][j]==0){
-                    matrix[i][j]=0;
+        }
+      
+          for(int i=1;i<arr.length;i++){
+            for(int j=1;j<arr[0].length;j++){
+                if(arr[i][0]==0||arr[0][j]==0){
+                    arr[i][j]=0;
                 }
             }
           }
         
-// handle first row FIRST
-if (matrix[0][0] == 0) {
-    for (int j = 0; j < matrix[0].length; j++) {
-        matrix[0][j] = 0;
-    }
+
+            if (arr[0][0]==0) {
+            for (int j=0;j<arr[0].length; j++) {
+            arr[0][j]=0;
+            }
 }
 
-// handle first column AFTER
-if (col == 0) {
-    for (int i = 0; i < matrix.length; i++) {
-        matrix[i][0] = 0;
-    }
+                   if (co==0) {
+                     for (int i = 0; i < arr.length; i++) {
+                     arr[i][0] = 0;
+                     }
 }
 
-
+          
     }
 }
