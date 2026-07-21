79a6e62ed8250e97395a589c3e9dbec5901e1795
Pulkit <choprapulkitt@gmail.com>
2026-07-21 14:09:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (82.56%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,23 +1,35 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
-        int s=0, e=nums.length-1;
-
-        while (s <= e) {
+    public boolean search(int[] arr, int k) {
+        int n = arr.length;
+        int s =0;
+        int e = n-1;
+        while(s<=e){
             int mid = s+(e-s)/2;
-
-            if (nums[mid]==target) return true;
-
-            if (nums[s]==nums[mid]&&nums[mid]==nums[e]) {
-                s++;
-                e--;
-            } 
-            else if (nums[s]<=nums[mid]) {
-                if (nums[s]<=target&&target<nums[mid])e=mid-1;
-                else s=mid+1;
-            } 
-            else {
-                if (nums[mid]<target&&target<=nums[e]) s=mid+1;
-                else e=mid-1;
+            if(arr[mid]==k){
+                return true;
+            } else {
+                while(s<=e && arr[s]==arr[e] ){
+                    if(arr[s]==k) return true;
+                    s++;
+                    e--;
+                }
+                if(s>e){
+                    break;
+                }
+                // find sorted one
+                if(arr[s]<=arr[mid]){
+                   if(k>=arr[s] && k<arr[mid]){
+                    e=mid-1;
+                   } else {
+                    s=mid+1;
+                   }
+                } else {
+                    if(k>=arr[mid] && k<=arr[e]){
+                    s=mid+1;
+                   } else {
+                    e=mid-1;
+                   }
+                }
             }
         }
         return false;
