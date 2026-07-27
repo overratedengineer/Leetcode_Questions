3ff49536b1577be06b6c3251c06f1b3cba006dea
Pulkit <choprapulkitt@gmail.com>
2026-07-27 15:16:53 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (58.08%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,63 +10,50 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode temp = l1;
+        // add till any lsit become null take carry 
+        //proceed carry to next store it as prev 
+        //carry can be seem as n/10 and the sum unit digit will be n%10
+        ListNode temp1=l1;
         ListNode temp2 =l2;
+        ListNode fin = new ListNode(1);
+        ListNode dummy = fin ;
         int carry =0;
-        while(temp!=null && temp2!=null){
-            int val1 = temp.val;
-            int val2 = temp2.val;
-            int fv = val1+val2+carry;
-            if(fv>9){
-                carry=1;
-                fv%=10;
-            } else {
-                carry=0;
-            }
-            temp.val=fv;
-            temp2.val=fv;
-            temp=temp.next;
-            temp2=temp2.next;
+        while(temp1!=null && temp2!=null){
+            int v1 = temp1.val;
+            int v2 = temp2.val;
+            int sum = v1+v2+carry;
+            int tokeep = sum%10;
+              ListNode c = new ListNode(tokeep);
+              dummy.next=c;
+              dummy=dummy.next;
+            carry = sum/10;
+            temp1=temp1.next;
+             temp2=temp2.next;
         }
-       ListNode head= null;
-        if(temp==null){
-            head=l2;
-        } else {
-            head=l1;
+        while(temp1!=null){
+             int v1 = temp1.val;
+            int sum = v1+carry;
+            int tokeep = sum%10;
+            ListNode c = new ListNode(tokeep);
+            dummy.next=c;
+            dummy=dummy.next;
+            carry = sum/10;
+              temp1=temp1.next;
+            
         }
-        while(temp!=null){
-            int val1 = temp.val;
-            int fv = val1+carry;
-             if(fv>9){
-                carry=1;
-                fv%=10;
-            } else {
-                carry=0;
-            }
-            temp.val=fv;
-            temp=temp.next;
-        }
-        while(temp2!=null){
-            int val1 = temp2.val;
-            int fv = val1+carry;
-             if(fv>9){
-                carry=1;
-                fv%=10;
-            } else {
-                carry=0;
-            }
-            temp2.val=fv;
-            temp2=temp2.next;
+         while(temp2!=null){
+             int v1 = temp2.val;
+            int sum = v1+carry;
+            int tokeep = sum%10;
+            ListNode c = new ListNode(tokeep);
+            dummy.next=c;
+            dummy=dummy.next;
+            carry = sum/10;
+              temp2=temp2.next;
         }
         if(carry!=0){
-            ListNode t = head;
-            while(t.next!=null){
-                t=t.next;
-            }
-            ListNode n = new ListNode(carry);
-            t.next=n;
-            n.next=null;
+            dummy.next= new ListNode(carry);
         }
-       return head;
+        return fin.next;
     }
 }
