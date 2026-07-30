d828ae7b608ac64153543349da1599df9abe403c
Pulkit <choprapulkitt@gmail.com>
2026-07-30 21:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (75.26%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -9,54 +9,50 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head){
-        ListNode temp=head;
-        ListNode prev = null;
-        ListNode h2 = null;
-        while(temp!=null){
-            if(temp.next==null) h2 = temp;
-           ListNode next = temp.next;
-           temp.next=prev;
-           prev=temp;
-           temp=next;
-        } 
-        return h2;
-    }
-    public ListNode kn(ListNode head ,int k){
-        int count = 1;
-        ListNode temp=head;
-        while(temp!=null && count<k){
-            temp=temp.next;
-            count++;
-        }
-        if(count==k) 
+    public ListNode kn(ListNode head,int k){
+        ListNode temp = head;
+        int count =1;
+      while(temp!=null && count<k){
+         temp=temp.next;
+         count++;
+      }
+      if(count>=k){
         return temp;
-        else 
+      } else {
         return null;
+      }
     }
-    public ListNode reverseKGroup(ListNode head, int k) {
-        // first is to find k elements and pass it to reverse
-       //1-2-3 4-5-6 7-8-9
-       ListNode temp = head;
+    public ListNode reverse(ListNode head){
        ListNode prev = null;
-       ListNode next = null;
+       ListNode temp = head;
        while(temp!=null){
-         ListNode knode = kn(temp,k);
-         if(knode==null){
-         if(prev!=null) prev.next=temp;
-         break;
-         }
-         next = knode.next;
-         knode.next=null;
-         reverse(temp);
-         if(temp==head){
-            head=knode;
-         } else {
-         prev.next = knode;
-         }  
+        ListNode next = temp.next;
+         temp.next = prev;
          prev=temp;
          temp=next;
        }
-       return head;
+       return prev;
     }
-}       
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode next =null;
+        while(temp!=null){
+            ListNode knode = kn(temp,k);
+            if(knode==null){
+             if(prev!=null)prev.next = temp;
+              break;
+            } 
+            if(temp==head){
+                head=knode;
+            }
+             next = knode.next;
+             knode.next = null;
+             reverse(temp);
+             if(prev!=null) prev.next = knode;
+             prev = temp;
+             temp=next;
+        }
+        return head;
+    }
+}
