784afe3dd47b49c4d30ce5a0b27349dd99134944
Pulkit <choprapulkitt@gmail.com>
2026-07-31 12:54:17 +0530

    Time: 9 ms (94.22%) | Memory: 48.2 MB (17.34%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,26 +1,21 @@
 class Solution {
     public int minimumPushes(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((e1, e2) -> {
-            if (!e1.getValue().equals(e2.getValue()))
-                return e2.getValue() - e1.getValue();
-            return e1.getKey() - e2.getKey();
-        });
-        int count = 0;
-        int mul = 1;
+       int[] freq = new int[26];
+       for (char c : s.toCharArray()) {
+       freq[c - 'a']++;
+       }
+       Arrays.sort(freq);
         int ans = 0;
-        for (Map.Entry<Character, Integer> e : list) {
-            if (count == 8) {
-                count = 0;
+        int mul = 1;
+        int cnt = 0;
+        for (int i = 25; i >= 0 && freq[i] > 0; i--) {
+            if (cnt == 8) {
+                cnt = 0;
                 mul++;
             }
-            count++;
-            ans += e.getValue() * mul;
+            ans += freq[i] * mul;
+            cnt++;
         }
-        return ans;
+return ans;
     }
 }
