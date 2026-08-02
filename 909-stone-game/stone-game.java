66f71dca6a63bd32767b633bd8b88f8c51c8a162
Pulkit <choprapulkitt@gmail.com>
2026-08-02 22:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (88.59%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,15 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        Integer dp[][] = new Integer[n][n];
-        return rec(0,n-1,piles,dp)>0;
-    }
-    public int rec(int l,int r,int arr[], Integer dp[][])
-    {
-        if(l==r) return arr[l];
-        if(dp[l][r]!=null) return dp[l][r];
-        int m = arr[l] + rec(l+1,r,arr,dp);
-        int o = arr[r]+rec(l,r-1,arr,dp);
-        return dp[l][r]=Math.max(m,o);
-    }
+       return true;
+       }
 }
