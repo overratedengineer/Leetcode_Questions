ab3c7f251225d552b9e05e65674dccfdec18bf2f
Pulkit <choprapulkitt@gmail.com>
2026-08-08 18:18:53 +0530

    Time: 3 ms (86.01%) | Memory: 43.4 MB (26.05%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,25 +2,20 @@ import java.util.*;
 
 class Solution {
     public boolean isValid(String s) {
-
         Stack<Character> st = new Stack<>();
-
         for(char c : s.toCharArray()){
-
             if(c == '}'){
                 if(!st.isEmpty()){
                     char d = st.pop();
                     if(d != '{') return false;
                 } else return false;
             }
-
             else if(c == ')'){
                 if(!st.isEmpty()){
                     char d = st.pop();
                     if(d != '(') return false;
                 } else return false;
             }
-
             else if(c == ']'){
                 if(!st.isEmpty()){
                     char d = st.pop();
