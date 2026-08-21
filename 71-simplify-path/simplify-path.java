dd82013629ca7c97a75398d82d87a15f9face3f4
Pulkit <choprapulkitt@gmail.com>
2026-08-22 02:30:35 +0530

    Time: 10 ms (6.31%) | Memory: 46.4 MB (8.97%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String simplifyPath(String s) {
+        String[] arr = s.split("/+");
+        StringBuilder ans = new StringBuilder();
+        Stack<String> st = new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].equals("..")){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            } else if(!arr[i].equals(".") && !arr[i].equals("")) {
+                st.push(arr[i]);
+            }
+        }
+        while(!st.isEmpty()){
+            ans.append(new StringBuilder(st.pop()).reverse().toString()+"/");
+        }
+       ans.reverse();
+    //    if(ans.length()>0) {
+    //      ans.deleteCharAt(0);
+    //     }
+         if(ans.length() == 0) {
+            return "/";
+        }
+       return ans.toString();
+    }
+}
